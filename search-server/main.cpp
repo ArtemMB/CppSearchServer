f3563fa94b2898cf372e6c9f4b589447974cf795
ArtemMB <artemmb@rambler.ru>
2022-02-02 21:13:50 +0300

    + Причесал

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -277,17 +277,18 @@ private:
 
 // Тест проверяет, что поисковая система исключает стоп-слова при добавлении документов
 void TestExcludeStopWordsFromAddedDocumentContent() {
-    const int doc_id = 42;
-    const string content = "cat in the city"s;
-    const vector<int> ratings = {1, 2, 3};
+    const int doc_id{42};
+    const string content{"cat in the city"s};
+    const vector<int> ratings{ 1, 2, 3};
+    
     // Сначала убеждаемся, что поиск слова, не входящего в список стоп-слов,
     // находит нужный документ
     {
         SearchServer server;
         server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
-        const auto found_docs = server.FindTopDocuments("in"s);
+        const auto found_docs{ server.FindTopDocuments("in"s)};
         assert(found_docs.size() == 1);
-        const Document& doc0 = found_docs[0];
+        const Document& doc0{found_docs[0]};
         assert(doc0.id == doc_id);
     }
 
@@ -309,6 +310,7 @@ void TestExcludeStopWordsFromAddedDocumentContent() {
 void TestSearchServer() {
     TestExcludeStopWordsFromAddedDocumentContent();
     // Не забудьте вызывать остальные тесты здесь
+    cout<<"finished";
 }
 
 // --------- Окончание модульных тестов поисковой системы -----------
