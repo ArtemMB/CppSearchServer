884e45f8040161203b3bb87eff96c928e0d54fee
ArtemMB <artemmb@rambler.ru>
2022-02-02 20:26:55 +0300

    + Подготовка к unit тестированию

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -7,6 +7,7 @@
 #include <utility>
 #include <vector>
 #include <numeric>
+#include <cassert>
 
 #include <windows.h>
 
@@ -272,7 +273,45 @@ private:
     }       
 };
 
+// -------- Начало модульных тестов поисковой системы ----------
 
+// Тест проверяет, что поисковая система исключает стоп-слова при добавлении документов
+void TestExcludeStopWordsFromAddedDocumentContent() {
+    const int doc_id = 42;
+    const string content = "cat in the city"s;
+    const vector<int> ratings = {1, 2, 3};
+    // Сначала убеждаемся, что поиск слова, не входящего в список стоп-слов,
+    // находит нужный документ
+    {
+        SearchServer server;
+        server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
+        const auto found_docs = server.FindTopDocuments("in"s);
+        assert(found_docs.size() == 1);
+        const Document& doc0 = found_docs[0];
+        assert(doc0.id == doc_id);
+    }
+
+    // Затем убеждаемся, что поиск этого же слова, входящего в список стоп-слов,
+    // возвращает пустой результат
+    {
+        SearchServer server;
+        server.SetStopWords("in the"s);
+        server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
+        assert(server.FindTopDocuments("in"s).empty());
+    }
+}
+
+/*
+Разместите код остальных тестов здесь
+*/
+
+// Функция TestSearchServer является точкой входа для запуска тестов
+void TestSearchServer() {
+    TestExcludeStopWordsFromAddedDocumentContent();
+    // Не забудьте вызывать остальные тесты здесь
+}
+
+// --------- Окончание модульных тестов поисковой системы -----------
 void PrintDocument(const Document& document) {
     cout << "{ "s
          << "document_id = "s << document.id << ", "s
@@ -285,7 +324,9 @@ int main() {
     //выключить в конcоли винды utf-8
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
-            
+    
+    TestSearchServer();
+    /*
     SearchServer search_server;
     search_server.SetStopWords("и в на"s);
 
@@ -313,7 +354,7 @@ int main() {
                                         [](int document_id,    DocumentStatus, int )
     { return document_id % 2 == 0; })) {
         PrintDocument(document);
-    }    
+    }    */
 
     return 0;
 }
