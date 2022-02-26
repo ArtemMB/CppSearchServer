2de38c6b9a621c1e6995db4c5bcacb9b9a8ae492
ArtemMB <artemmb@rambler.ru>
2022-02-26 15:04:19 +0300

    + Реализовал задание

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -454,45 +454,58 @@ auto Paginate(const Container& c, size_t page_size) {
    
 class RequestQueue {
 public:
-    explicit RequestQueue(const SearchServer& search_server):
-        search_server_{search_server}
-    {        
+    explicit RequestQueue(const SearchServer& search_server)
+        : search_server_(search_server)
+        , no_results_requests_(0)
+        , current_time_(0) {
     }
-        
-    // сделаем "обёртки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
+    // сделаем "обертки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
     template <typename DocumentPredicate>
-    vector<Document> AddFindRequest(const string& raw_query, 
-                                    DocumentPredicate document_predicate) {
-        // напишите реализацию
-        auto search_results = search_server_.FindTopDocuments(raw_query, 
-                                                              document_predicate);
-        return search_results;
+    vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate document_predicate) {
+        const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
+        AddRequest(result.size());
+        return result;
     }
-
-    vector<Document> AddFindRequest(const string& raw_query, DocumentStatus status) {        
-        return AddFindRequest(
-            raw_query,
-            [status](int document_id, DocumentStatus document_status, int rating) {
-                return document_status == status;
-            });
+    vector<Document> AddFindRequest(const string& raw_query, DocumentStatus status) {
+        const auto result = search_server_.FindTopDocuments(raw_query, status);
+        AddRequest(result.size());
+        return result;
     }
-
-    vector<Document> AddFindRequest(const string& raw_query) {        
-        return AddFindRequest(raw_query, DocumentStatus::ACTUAL);
+    vector<Document> AddFindRequest(const string& raw_query) {
+        const auto result = search_server_.FindTopDocuments(raw_query);
+        AddRequest(result.size());
+        return result;
     }
-
     int GetNoResultRequests() const {
-        // напишите реализацию
+        return no_results_requests_;
     }
 private:
     struct QueryResult {
-        // определите, что должно быть в структуре
+        uint64_t timestamp;
+        int results;
     };
-    
     deque<QueryResult> requests_;
-    const static int min_in_day_ = 1440;
-    // возможно, здесь вам понадобится что-то ещё
     const SearchServer& search_server_;
+    int no_results_requests_;
+    uint64_t current_time_;
+    const static int min_in_day_ = 1440;
+ 
+    void AddRequest(int results_num) {
+        // новый запрос - новая секунда
+        ++current_time_;
+        // удаляем все результаты поиска, которые устарели
+        while (!requests_.empty() && min_in_day_ <= current_time_ - requests_.front().timestamp) {
+            if (0 == requests_.front().results) {
+                --no_results_requests_;
+            }
+            requests_.pop_front();
+        }
+        // сохраняем новый результат поиска
+        requests_.push_back({current_time_, results_num});
+        if (0 == results_num) {
+            ++no_results_requests_;
+        }
+    }
 };
 
 int main() {
