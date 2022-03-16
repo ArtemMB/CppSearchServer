8c3221b6d0d2926fdd37967788b41d9189f59b54
ArtemMB <artemmb@rambler.ru>
2022-03-16 20:39:48 +0300

    + Убрал мусор

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,12 +1,7 @@
 #include <windows.h>
 
-#include "document.h"
-#include "read_input_functions.h"
 #include "search_server.h"
-#include "paginator.h"
-#include "request_queue.h"
 #include "remove_duplicates.h"
-
 #include "test_example_functions.hpp"
 
 using namespace std;
