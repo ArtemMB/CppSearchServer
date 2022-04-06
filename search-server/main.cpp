d0921d39309824009f093fa9b7d6af01005f7492
ArtemMB <artemmb@rambler.ru>
2022-04-06 23:21:03 +0300

    + Сдал задачу  Параллелим запросы к поисковой системе Задание 2/2

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -70,7 +70,17 @@ int main() {
     
     TestProcessQueriesJoined();
     
-    
+    mt19937 generator;
+    const auto dictionary = GenerateDictionary(generator, 2'000, 25);
+    const auto documents = GenerateQueries(generator, dictionary, 20'000, 10);
+
+    SearchServer search_server(dictionary[0]);
+    for (size_t i = 0; i < documents.size(); ++i) {
+        search_server.AddDocument(i, documents[i], DocumentStatus::ACTUAL, {1, 2, 3});
+    }
+
+    const auto queries = GenerateQueries(generator, dictionary, 2'000, 7);
+    TEST(ProcessQueriesJoined);
     
     return 0;
 }
